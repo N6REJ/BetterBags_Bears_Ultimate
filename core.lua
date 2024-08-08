ed1021527652af93a1e429fdc94587b21b43791a
Bear <programming@hallhome.us>
2024-08-07 23:37:56 -0500

    Update README.md and core.lua for BetterBags Bears Ultimate; update .toc file with new database entries

--- core.lua
+++ core.lua
@@ -1,8 +1,8 @@
 -- Created by @project-author@ character is Bearesquishy - dalaran please credit whenever.
 -- Source on GitHub: https://n6rej.github.io
 
----@type string, BBBU
-local _, addon = ...
+---@types
+local _, addonTable = ...
 ---@class BetterBags: AceAddon
 local BetterBags = LibStub('AceAddon-3.0'):GetAddon("BetterBags")
 ---@class Categories: AceModule
@@ -20,21 +20,9 @@ local function parseItems(db)
 end
 
 -- START HERE
--- This will cycle thru each expansion listed in expansion.lua and run the database for that expansion.
-for key, value in pairs(addonTable.expansion) do
-	db = addonTable[value]
-	if (db ~= nil) then
+-- This will cycle through each database listed in addonTable and run the database for that expansion.
+for dbName, db in pairs(addonTable) do
+	if type(db) == "table" then
 		parseItems(db)
-	end
-end
-
--- Check if globaldbs is being used in toc
-if (addonTable.Globaldbs ~= nil) then
-	-- Now process global databases
-	for key, value in pairs(addonTable.Globaldbs) do
-		db = addonTable[value]
-		if (db ~= nil) then
-			parseItems(db)
-		end
 	end
 end
