943e4d7be44a14888ec7b5de162a084956745b87
Bear <programming@hallhome.us>
2024-08-08 01:42:11 -0500

    Refactor `core.lua` to streamline item parsing by removing duplicate item detection and error handling for invalid items Ready for packaging

--- core.lua
+++ core.lua
@@ -11,25 +11,16 @@ local categories = BetterBags:GetModule('Categories')
 local L = BetterBags:GetModule('Localization')
 
 local function parseItems(db)
-    local seenItems = {}
     for category, items in pairs(db) do
         for _, item in pairs(items) do
-            if C_Item.GetItemInfoInstant(item) == nil then
-                print("oops... error invalid item", item)
-            else
-                if seenItems[item] then
-                    print("Duplicate item found:", item)
-                else
-                    seenItems[item] = true
-                    categories:AddItemToCategory(item, L:G(category))
-                end
-            end
+            categories:AddItemToCategory(item, L:G(category))
         end
     end
 end
 
+-- START HERE
 -- This will cycle through each database listed in addonTable and run the database for that expansion.
-for dbName, db in pairs(addonTable) do
+for dbName, db in pairs(addonTable.Database) do
     if type(db) == "table" then
         parseItems(db)
     end
