2dff2afd4c141d3ab3e8fd07d880e98ed724bfe9
Bear <programming@hallhome.us>
2024-08-08 20:16:50 -0500

    Update `Keystones.lua` to correct color code for Keystones category

--- Databases/Global/Keystones.lua
+++ Databases/Global/Keystones.lua
@@ -7,7 +7,7 @@ local _, addonTable = ...
 -- Database
 -----------------------------------------------------------
 local db = {
-    ["|cff16B7FFHearthstones|r"] = {
+    ["|cff16a7FKeystones|r"] = {
         -- ID,	--Item name
         138019, -- Legion Keystone
         151086, -- Tournament Stone
