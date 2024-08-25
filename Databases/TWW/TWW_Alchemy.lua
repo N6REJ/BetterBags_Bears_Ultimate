c410b6e872be86e3b49a97da8c95527bdf9e94d9
Bear <programming@hallhome.us>
2024-08-24 19:54:45 -0500

    Add new items to TWW_Alchemy.lua database including Arathor's Spear and Mycobloom

--- Databases/TWW/TWW_Alchemy.lua
+++ Databases/TWW/TWW_Alchemy.lua
@@ -12,6 +12,13 @@ local db = {
         210805,    -- blessing Blossom 1
         210806,    -- blessing Blossom 2
         210807,    -- blessing Blossom 3
+        210808,    -- Arathor's Spear 1
+        210809,    -- Arathor's Spear 3
+        210810,    -- Arathor's Spear 3
+        210796,    -- Mycobloom 1
+        210797,    -- Mycobloom 2
+        210798,    -- Mycobloom 3
+
     }
 }
 -- now that db is populated lets pass it on.
