86603a58e28e3d78ab27e0f23541e40f9b7118fd
Bear <programming@hallhome.us>
2024-08-24 20:39:37 -0500

    Add `TWW_Ultimate.lua` database and update `.toc` file to include it

---
+++ Databases/TWW/TWW_Ultimate.lua
@@ -0,0 +1,15 @@
+-- Created by @project-author@ character is Bearesquishy - dalaran please credit whenever used.
+-- Source on GitHub: https://n6rej.github.io
+
+---@type
+local _, addonTable = ...
+
+-- Database
+local db = {
+    ["Knowledge"] = {
+        -- ID,	--Item name
+        226334, -- Earthen Excavator's Shovel
+    }
+}
+-- now that db is populated lets pass it on.
+addonTable.Database.TWWUltimate = db
