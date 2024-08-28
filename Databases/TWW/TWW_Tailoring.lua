b80bc18140de10ae7c5bd0b2554e885d1e5b447d
Bear <programming@hallhome.us>
2024-08-28 02:50:25 -0500

    Add new thread and crafted items to `TWW_Tailoring.lua` database

--- Databases/TWW/TWW_Tailoring.lua
+++ Databases/TWW/TWW_Tailoring.lua
@@ -12,6 +12,19 @@ local db = {
         222804, -- Weavercloth Bolt 1
         222805, -- Weavercloth Bolt 2
         222806, -- Weavercloth Bolt 3
+        222789, -- Spool of Duskthread 1
+        222790, -- Spool of Duskthread 2
+        222791, -- Spool of Duskthread 3
+        222792, -- Spool of Dawnthread 1
+        222793, -- Spool of Dawnthread 2
+        222794, -- Spool of Dawnthread 3
+        222795, -- Spool of Weaverthread 1
+        222796, -- Spool of Weaverthread 2
+        222797, -- Spool of Weaverthread 3
+    },
+    ["Crafted"] = {
+        -- ID,	--Item name
+        224832, -- Exquisite Weavercloth Bolt
     }
 }
 -- now that db is populated lets pass it on.
