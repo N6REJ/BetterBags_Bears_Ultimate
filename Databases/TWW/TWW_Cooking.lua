d84433c9b8d600b1dae52fc5d6543103e9a747ec
Bear <programming@hallhome.us>
2025-01-22 06:12:16 -0600

    Move Food category from TWW_Ultimate.lua to TWW_Cooking.lua database

--- Databases/TWW/TWW_Cooking.lua
+++ Databases/TWW/TWW_Cooking.lua
@@ -39,6 +39,52 @@ local db = {
         225568, -- Slum Shark Scrap
         222738, -- Portioned Steak
         222739, -- Spiced Meat Stock
+    },
+    ["Food"] = {
+        222751, -- Hearty Simple Stew
+        225592, -- Exquisitely Eviscerated Muscle
+        222702, -- Skewered Fillet
+        222703, -- Simple Stew
+        222704, -- Unseasoned Field Steak
+        222705, -- Roasted Mycobloom
+        222706, -- Pan Seared Mycobloom
+        222707, -- Hallowfall Chili
+        222708, -- Coreway Kabob
+        222709, -- Flash Fire Fillet
+        222710, -- Meat and Potatoes
+        222711, -- Rib Stickers
+        222712, -- Sweet and Sour Meatballs
+        222713, -- Tender Twilight Jerky
+        222714, -- Zesty Nibblers
+        222715, -- Fiery Fish Sticks
+        222716, -- Ginger Glazed Fillet
+        222717, -- Salty Dog
+        222718, -- Deepfin Patty
+        222719, -- Sweet and Spicy Soup
+        222720, -- The Sushi Special
+        222721, -- Fish and Chips
+        222722, -- Salt Baked Seafood
+        222723, -- Marinated Tenderloins
+        222724, -- Sizzling Honey Roast
+        222725, -- Mycobloom Risotto
+        222726, -- Stuffed Cave Peppers
+        222727, -- Angler's Delight
+        222728, -- Beledar's Bounty
+        222729, -- Empress' Farewell
+        222730, -- Jester's Board
+        222731, -- Outsider's Provisions
+        222735, -- Everything Stew
+        222736, -- Chippy Tea
+        222744, -- Cinder Nectar
+        222745, -- Pep-In-Your-Step
+        222747, -- Rockslide Shake
+        222748, -- Sticky Sweet Treat
+        222749, -- Melted Candlebar
+        223968, -- Spongey Scramble
+        223970, -- Little Buddy Biscuits
+        223971, -- Azj-Kahet Special
+        225855, -- Ghoulfish Delight
+        222776, -- Hearty Beledar's Bounty
     }
 }
 -- now that db is populated lets pass it on.
--- Databases/TWW/TWW_Ultimate.lua
+++ Databases/TWW/TWW_Ultimate.lua
@@ -298,51 +298,6 @@ local db = {
         224572, -- Crystallized Augment Rune
         211495, -- Dreambound Augment Rune
     },
-    ["Food"] = {
-        222751, -- Hearty Simple Stew
-        225592, -- Exquisitely Eviscerated Muscle
-        222702, -- Skewered Fillet
-        222703, -- Simple Stew
-        222704, -- Unseasoned Field Steak
-        222705, -- Roasted Mycobloom
-        222706, -- Pan Seared Mycobloom
-        222707, -- Hallowfall Chili
-        222708, -- Coreway Kabob
-        222709, -- Flash Fire Fillet
-        222710, -- Meat and Potatoes
-        222711, -- Rib Stickers
-        222712, -- Sweet and Sour Meatballs
-        222713, -- Tender Twilight Jerky
-        222714, -- Zesty Nibblers
-        222715, -- Fiery Fish Sticks
-        222716, -- Ginger Glazed Fillet
-        222717, -- Salty Dog
-        222718, -- Deepfin Patty
-        222719, -- Sweet and Spicy Soup
-        222720, -- The Sushi Special
-        222721, -- Fish and Chips
-        222722, -- Salt Baked Seafood
-        222723, -- Marinated Tenderloins
-        222724, -- Sizzling Honey Roast
-        222725, -- Mycobloom Risotto
-        222726, -- Stuffed Cave Peppers
-        222727, -- Angler's Delight
-        222728, -- Beledar's Bounty
-        222729, -- Empress' Farewell
-        222730, -- Jester's Board
-        222731, -- Outsider's Provisions
-        222735, -- Everything Stew
-        222736, -- Chippy Tea
-        222744, -- Cinder Nectar
-        222745, -- Pep-In-Your-Step
-        222747, -- Rockslide Shake
-        222748, -- Sticky Sweet Treat
-        222749, -- Melted Candlebar
-        223968, -- Spongey Scramble
-        223970, -- Little Buddy Biscuits
-        223971, -- Azj-Kahet Special
-        225855, -- Ghoulfish Delight
-    },
     ["Feast"] = {
         222732, -- Feast of the Divine Day
         222733, -- Feast of the Midnight Masquerade
