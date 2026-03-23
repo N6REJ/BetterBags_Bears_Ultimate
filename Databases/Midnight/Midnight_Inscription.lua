12a91db35accc47b550d95507d06539169bc85c9
Bear <programming@hallhome.us>
2026-03-23 04:23:49 -0500

    inscription

---
+++ Databases/Midnight/Midnight_Inscription.lua
@@ -0,0 +1,80 @@
+-- Created by @project-author@ character is Bearesquishy - dalaran please credit whenever used.
+-- Source on GitHub: https://n6rej.github.io
+
+---@type
+local _, addonTable = ...
+
+-- Database
+
+local db = {
+    ["Pigment"] = {
+        245807, -- Powder Pigment
+        245808, -- Powder Pigment
+        245803, -- Argentleaf Pigment
+        245839, -- Sanguithorn Pigment
+        245842, -- Sanguithorn Pigment
+        245836, -- Mana Lily Pigment
+        245844, -- Mana Lily Pigment
+        245837, -- Argentleaf Pigment
+    },
+    ["Ink"] = {
+        245801, -- Munsell Ink
+        245805, -- Sienna Ink
+        245847, -- Munsell Ink
+        245806, -- Sienna Ink
+    },
+    ["Cards"] = {
+        -- Blood
+        245838, -- Ace of Blood
+        245857, -- Two of Blood
+        245860, -- Five of Blood
+        245861, -- Six of Blood
+        245859, -- Four of Blood
+        245850, -- Seven of Blood
+        245863, -- Eight of Blood
+
+        -- Void
+        245838, -- Ace of Void
+        245839, -- Two of Void
+        245840, -- Three of Void
+        245841, -- Four of Void
+        245842, -- Five of Void
+        245843, -- Six of Void
+        245844, -- Seven of Void
+        245845, -- Eight of Void
+
+        -- Hunt
+        245830, -- Ace of Hunt
+        245835, -- Six of Hunt
+        245831, -- Two of Hunt
+        245833, -- Four of Hunt
+        245843, -- Three of Hunt
+        245845, -- Five of Hunt
+        245851, -- Seven of Hunt
+        245864, -- Eight of Hunt
+
+        -- Rot
+        245847, -- Ace of Rot
+        245848, -- Two of Rot
+        245849, -- Three of Rot
+        245850, -- Four of Rot
+        245851, -- Five of Rot
+        245852, -- Six of Rot
+        245853, -- Seven of Rot
+        245854, -- Eight of Rot
+    },
+    ["Embellishment"] = {
+        251923, -- Thalassian Essence of the Faire
+        245764, -- Codified Azeroot
+        245881, -- Lexicologist's Vellum
+        245882, -- Thalassian Songwater
+        245765, -- Codified Azeroot
+        245804, -- Soul Cipher
+        245802, -- Soul Cipher
+    },
+    ["Vendor"] = {
+        245881, -- Lexicologist's Vellum
+    }
+}
+-- now that db is populated lets pass it on.
+addonTable.Database.MidnightInscription = db
