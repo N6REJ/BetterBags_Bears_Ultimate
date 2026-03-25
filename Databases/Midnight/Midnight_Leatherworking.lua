f7264efa3462515b6007e6dd4e372f96120d3f49
Bear <programming@hallhome.us>
2026-03-25 03:35:00 -0500

    Add Void-Tempered and Scalewoven materials to Midnight Leatherworking database

--- Databases/Midnight/Midnight_Leatherworking.lua
+++ Databases/Midnight/Midnight_Leatherworking.lua
@@ -9,7 +9,28 @@ local _, addonTable = ...
 local db = {
     ["Leather"] = {
         -- ID,	--Item name
-        238511, -- Void-tempered Leather
+        238529, -- Majestic Hide
+        238528, -- Majestic Claw
+        238530, -- Majestic Fin
+        238525, -- Fantastic Fur
+        238513, -- Void-Tempered Scales
+        238522, -- Peerless Plumage
+        238511, -- Void-Tempered Leather
+        244633, -- Infused Scalewoven Hide
+        244637, -- Silvermoon Weapon Wrap
+        244635, -- Sin'dorei Armor Banding
+        238514, -- Void-Tempered Scales
+        238523, -- Carving Canine
+        238518, -- Void-Tempered Hide
+        238520, -- Void-Tempered Plating
+        244631, -- Scalewoven Hide
+        238512, -- Void-Tempered Leather
+        238519, -- Void-Tempered Hide
+        244634, -- Infused Scalewoven Hide
+        238521, -- Void-Tempered Plating
+        244632, -- Scalewoven Hide
+        244636, -- Sin'dorei Armor Banding
+        244638, -- Silvermoon Weapon Wrap
     },
     ["Crafted"] = {
         -- Crafted
@@ -17,7 +38,6 @@ local db = {
     },
     ["Embellishment"] = {
             -- ID,	--Item name
-
     }
 }
 -- now that db is populated lets pass it on.
