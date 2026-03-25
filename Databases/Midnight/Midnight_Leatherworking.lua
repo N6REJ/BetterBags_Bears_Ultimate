2b7e8923cb4c44a9e319930933c48d5354920532
Bear <programming@hallhome.us>
2026-03-25 03:41:02 -0500

    Reorganize Midnight Leatherworking database and fix Crafted item IDs

--- Databases/Midnight/Midnight_Leatherworking.lua
+++ Databases/Midnight/Midnight_Leatherworking.lua
@@ -9,32 +9,33 @@ local _, addonTable = ...
 local db = {
     ["Leather"] = {
         -- ID,	--Item name
+        238523, -- Carving Canine
+        238525, -- Fantastic Fur
+        244633, -- Infused Scalewoven Hide
+        244634, -- Infused Scalewoven Hide
         238529, -- Majestic Hide
         238528, -- Majestic Claw
         238530, -- Majestic Fin
-        238525, -- Fantastic Fur
-        238513, -- Void-Tempered Scales
         238522, -- Peerless Plumage
-        238511, -- Void-Tempered Leather
-        244633, -- Infused Scalewoven Hide
-        244637, -- Silvermoon Weapon Wrap
-        244635, -- Sin'dorei Armor Banding
-        238514, -- Void-Tempered Scales
-        238523, -- Carving Canine
-        238518, -- Void-Tempered Hide
-        238520, -- Void-Tempered Plating
         244631, -- Scalewoven Hide
-        238512, -- Void-Tempered Leather
-        238519, -- Void-Tempered Hide
-        244634, -- Infused Scalewoven Hide
-        238521, -- Void-Tempered Plating
         244632, -- Scalewoven Hide
+        244635, -- Sin'dorei Armor Banding
         244636, -- Sin'dorei Armor Banding
+        244637, -- Silvermoon Weapon Wrap
         244638, -- Silvermoon Weapon Wrap
+        238511, -- Void-Tempered Leather
+        238512, -- Void-Tempered Leather
+        238518, -- Void-Tempered Hide
+        238519, -- Void-Tempered Hide
+        238513, -- Void-Tempered Scales
+        238514, -- Void-Tempered Scales
+        238520, -- Void-Tempered Plating
+        238521, -- Void-Tempered Plating
     },
     ["Crafted"] = {
         -- Crafted
-        244638, -- Silvermoon Weapon Wrap
+        1237575, -- Silvermoon Weapon Wrap
+        1237574, -- Sin'dorei Armor Banding
     },
     ["Embellishment"] = {
             -- ID,	--Item name
