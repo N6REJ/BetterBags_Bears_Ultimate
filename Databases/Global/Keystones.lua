fb53b7fd398c78105c5c77769f3f63f384c6abaa
Bear <programming@hallhome.us>
2026-03-31 01:02:57 -0500

    Remove color code from Keystone category name in Global database

--- Databases/Global/Keystones.lua
+++ Databases/Global/Keystones.lua
@@ -7,7 +7,7 @@ local _, addonTable = ...
 -- Database
 
 local db = {
-    ["|cff16a7FFKey|r"] = {
+    ["Key"] = {
         -- ID,	--Item name
         138019, -- Legion Keystone
         151086, -- Tournament Stone
